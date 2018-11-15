7980a6096707edb19b75649a1c15f86d5939a480
CesarHuaman17 <cesarhuaman171275@gmail.com>
2018-11-15 16:30:36 -0500

    clase login
    creacion de la clase login con campos usuarios y clave

---
+++ BOL/login.php
@@ -0,0 +1,16 @@
+<?php
+class Login
+{
+	private $usuario;
+	private $clave;
+
+	public function __GET($x)
+	{
+		return $this->$x;
+	}
+	public function __SET($x, $y)
+	{
+		return $this->$x = $y;
+	}
+}
+?>
