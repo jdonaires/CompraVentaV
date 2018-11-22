df13d4369f2176a48f153b2f1fea87dc891f176f
CesherTorres <medic.cesar.ct7@gmail.com>
2018-11-22 14:45:21 -0500

    formulario de boleta
    en este formulario se ha considerado la boleta tanto de compra y vena, donde se ha utilizado tanto los diseño etiquetas propias de la tabla(th, tr) y diseño

---
+++ DESIGNER/frmboleta.php
@@ -0,0 +1,79 @@
+<html>
+<head>
+    <title>TOMASSA_ELECTRICS</title>
+    <link rel="stylesheet" type="text/css" href="Style/styletabla.css">
+</head>
+  <body>
+<div>
+  <table class="egt" border=1 cellspacing=0 cellpadding=2 bordercolor="#70BDEC">
+
+    <caption>TOMASSA_ELECTRICS</caption>
+    <tbody >
+        <tr>
+              <th class="encabezado" colspan="7"> <font size="40">BOLETA DE VENTA</font></th>
+        </tr>
+    </tbody>
+        <tr>
+              <th colspan="2">Nombre: <input type="text" size="40" name="fname"></th>
+              <th colspan="2"><input type="text" name="fname"></th>
+              <th colspan="2">Chincha: <input type="text" size="10" name="fname">de <input type="text" size="10" name="fname">
+               del <input type="text" size="10" name="fname"></th>
+        </tr>
+
+        <tr>
+              <th colspan="2">Apellido: <input type="text" size="40" name="fname"></th>
+              <td colspan="2"><input type="text" name="fname"></td><td colspan="2" rowspan="2">R.U.C:<input type="text" size="40" name="fname">
+              <br>BOLETA<br><input type="text" size="40" name="fname" ></td>
+        </tr>
+
+        <tr>
+              <th colspan="2">Direccion: <input type="text" size="40" name="fname"></th>
+              <th colspan="2"><input type="text" name="fname"></th>
+        </tr>
+
+        <tr class="Coloreado">
+              <th>Cantidad</th><th>Prod.Id</th><th colspan="2">Decripcion del Producto</th>
+              <th>Precio Unitario</th><th>Total</th>
+        </tr>
+
+        <tr>
+              <th><input type="text" name="fname"></th><th><input type="text" size="30" name="fname"></th><th colspan="2"><input type="text" size="40" name="fname"></th>
+              <th><input type="text" name="fname"></th><th><input type="text" name="fname"></th>
+        </tr>
+
+        <tr>
+              <th><input type="text" name="fname" ></th><th><input type="text" size="30" name="fname"></th><th colspan="2"><input type="text" size="40" name="fname"></th>
+              <th><input type="text" name="fname"></th><th><input type="text" name="fname"></th>
+        </tr>
+
+        <tr>
+              <th><input type="text" name="fname"></th><th><input type="text" size="30" name="fname"></th><th colspan="2"><input type="text" size="40" name="fname"></th>
+              <th><input type="text" name="fname"></th><th><input type="text" name="fname"></th>
+        </tr>
+
+        <tr>
+              <th><input type="text" name="fname"></th><th><input type="text" size="30" name="fname"></th><th colspan="2"><input type="text" size="40" name="fname"></th>
+              <th><input type="text" name="fname"></th><th><input type="text" name="fname"></th>
+        </tr>
+
+        <tr>
+              <th><input type="text" name="fname"></th><th><input type="text" size="30" name="fname"></th><th colspan="2"><input type="text" size="40" name="fname"></th>
+              <th><input type="text" name="fname"></th><th><input type="text" name="fname"></th>
+        </tr>
+
+        <tr>
+              <th class="GRACIAS" rowspan="3" colspan="4">GRACIAS POR SU COMPRA</th><th class="Sombreados">SUB TOTAL</th>
+              <td><input type="text" size="25" name="fname"></td>
+        </tr>
+
+        <tr>
+              <th class="Sombreados">IGV.......%</th><td><input type="text" size="25" name="fname"></td>
+        </tr>
+                <th class="Sombreados">TOTAL S/</th><td><input type="text" size="25" name="fname"></td>
+
+
+
+</table>
+</div>
+</body>
+</html>
