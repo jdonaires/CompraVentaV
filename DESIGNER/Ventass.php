a48262b25d93865e1b362646234e62d5534e183f
josethsaravia <josethsaravia13@gmail.com>
2018-11-22 19:38:29 -0500

    Ceacion del formulario VEntas
    Creacion e¿del formulario de ventas, y enlazamos

---
+++ DESIGNER/Ventass.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>TOMASSA_ELECTRICS</title>
+
+    <link rel="shortcut icon" href="imagenes/logo.png"/>
+    <link rel="stylesheet" type="text/css" href="Style/Posiciones.css">
+    <link rel="stylesheet" type="text/css" href="Style/StyleVentas.css">
+</head>
+  <body>
+
+    <form class="posicion_Ventas_1">
+    <label name="hola">ADMIN</label>
+    <img src="imagenes/ejecutivo1.png" width="30" height="30"/>
+
+  </form>
+
+  <form class="posicion_Ventas_2">
+      <h1>Datos</h1>
+      <hr/  width="100%" align="left">
+      <form>
+      <h4>Fecha:</h4>
+      <input  type="text" name="txtfecha" disabled="false" size="8%"  value="<?php echo date('Y-n-j'); ?>" />
+      <h4>Señor(es): </h4>
+      <input  type="text" name="nombreseñor" size="50%" >
+     </form>
+  </form>
+  <div class="posicion_Ventas_3">
+      <h1>Productos</h1>
+      <hr/  width="60%" align="left">
+      <h4>Nombre</h4>
+      <input  type="text" name="nombre" size="20%"/>
+      <input  type=image  src="Imagenes\lupa.png" width="30%" height="10%" >
+      <h4>Descripcion: </h4>
+      <input  type="text" disabled="false"  name="descripcion" size="50%" >
+      <h4>Precio: </h4>
+      <input  type="text" disabled="false" name="precio" size="50%" >
+      <h4>Cantidad: </h4>
+      <input  type="text" name="cantidad" size="10%" >
+      <input  type=image  src="Imagenes\logo.png" width="10%" height="10%" >
+    </div>
+
+    <fieldset class="posicion_Ventas_4">
+      <div>
+
+
+
+        </div>
+    </fieldset>
+
+
+
+</body>
+</html>
--- DESIGNER/frmprincipal.php
+++ DESIGNER/frmprincipal.php
@@ -14,9 +14,9 @@
     <ul class="menu">
         <li><a href="#" >  Inicio</a></li>
         <li><a href="frmRegistroEmpleado.php"> <img src="Imagenes/ejecutivo1.png"  width=30% height=30%  >Registro</a></li>
-        <li><a href="#"><img src="Imagenes/logo.png"  width=30% height=30%  > Ventas</a></li>
+        <li><a href="frmVentass.php"><img src="Imagenes/logo.png"  width=30% height=30%  > Ventas</a></li>
         <li><a href="#"><img src="Imagenes/compras.png"  width=30% height=30%   >Compras</a></li>
-        <li><a href="#"><img src="Imagenes/warehouse.png"  width=30% height=30%   >Almacen</a></li>
+        <li><a href="frmAlmacen.php"><img src="Imagenes/warehouse.png"  width=30% height=30%   >Almacen</a></li>
         <li><a href="frmLogin.php"><img src="Imagenes/salir.png"  width=30% height=30%   >Cerrar Sesión</a></li>
     </ul>
   </div>
